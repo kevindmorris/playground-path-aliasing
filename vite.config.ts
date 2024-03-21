4d7d1987cb9f55c845a0b3877fd30cb37cd02591
Morris, Kevin <Kevin.Morris@noblis.org>
2024-03-21 14:26:14 -0400

    update vite config with gh-pages base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/playground-path-aliasing/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
